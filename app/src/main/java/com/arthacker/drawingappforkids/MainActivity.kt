ecd8d63edd7252c759b4c1ac9c295ff022b5deff
arthacker8209 <2018csdeepak6816@poornima.edu.in>
2021-10-08 18:11:11 +0530

    Gallery Permission

--- app/src/main/java/com/arthacker/drawingappforkids/MainActivity.kt
+++ app/src/main/java/com/arthacker/drawingappforkids/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.arthacker.drawingappforkids
 
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Dialog
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.arthacker.drawingappforkids.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -55,6 +62,15 @@ class MainActivity : AppCompatActivity() {
         })
         brushDialog.show()
 
+        binding.chooseImage.setOnClickListener {
+            if (isReadStorageAllowed()){
+
+            }
+
+            else{
+                requestStoragePermission()
+            }
+        }
 
     }
 
@@ -75,77 +91,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-/*    @SuppressLint("CutPasteId")
-    private fun brushSizeDialog(){
-        val brushDialog = Dialog(this)
-        brushDialog.setContentView(R.layout.dialog_brush_size)
-        brushDialog.setTitle("Brush Size :")
 
-        val layout = findViewById<ConstraintLayout>(R.id.layout)
-        val small = layout?.findViewById<ImageButton>(R.id.ib_small_brush)
-        val medium = layout?.findViewById<ImageButton>(R.id.ib_medium_brush)
-        val large = layout?.findViewById<ImageButton>(R.id.ib_medium_brush)
-
-
-
-        small?.setOnClickListener {
-            binding.canvas.setSizeForBrush(10.toFloat())
-            brushDialog.dismiss()
+    private fun requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,  arrayOf( android.Manifest.permission.READ_EXTERNAL_STORAGE,
+          android.Manifest.permission.WRITE_EXTERNAL_STORAGE  ).toString())){
+            Toast.makeText(this, "Need Permission", Toast.LENGTH_SHORT).show()
         }
-        medium?.setOnClickListener {
-            binding.canvas.setSizeForBrush(20.toFloat())
-            brushDialog.dismiss()
+
+        ActivityCompat.requestPermissions(this, arrayOf( android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE  ), STORAGE_PERMISSION_CODE)
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode== STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Snackbar.make(binding.root, "Permission Granted", Snackbar.LENGTH_SHORT).show()
+            }
+            else{
+                Snackbar.make(binding.root, "You Denied Permission", Snackbar.LENGTH_SHORT).show()
+            }
         }
-        large?.setOnClickListener {
-            binding.canvas.setSizeForBrush(30.toFloat())
-            brushDialog.dismiss()
-        }
-        brushDialog.show()*/
+
+    }
+
+
+    private fun isReadStorageAllowed():Boolean{
+        val result = ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result== PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object{
+        private const val STORAGE_PERMISSION_CODE=111
+
     }
 
 
 
 
-/*
-import android.app.Dialog
-import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.get
-import com.arthacker.drawingappforkids.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.dialog_brush_size.*
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private var mImageButtonCurrentPaint: ImageButton? =
-        null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        drawing_view.setSizeForBrush(20.toFloat()) // Setting the default brush size to drawing view.
-
-
-        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
-        mImageButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(
-                this,
-                R.drawable.pallet_pressed
-            )
-        )
-        //END
-
-        ib_brush.setOnClickListener {
-            showBrushSizeChooserDialog()
-        }
     }
 
 
 
-}
-*/
+
